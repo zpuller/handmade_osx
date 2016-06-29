31e89b05e983570c38e475dd2c337ffdcf8e8880
Zach Puller <zachpuller@gmail.com>
2016-06-29 20:16:33 +0200

    fix spacing

--- handmade.cc
+++ handmade.cc
@@ -25,21 +25,21 @@ void RenderGradient(void* pixels, int width, int height, unsigned char xOffset, 
 
 void Initialize(GameMemory& memory)
 {
-    PermanentState zeroState = {};
-    memory.permanentState = zeroState;
-    memory.isInitialized = true;
+  PermanentState zeroState = {};
+  memory.permanentState = zeroState;
+  memory.isInitialized = true;
 }
 
 void GameUpdateAndRender(GameMemory& memory, GameOffscreenBuffer& offscreenBuffer, GameInput& input)
 {
-    if (input.wKey)
-      ++memory.permanentState.yOffset;
-    if (input.aKey)
-      ++memory.permanentState.xOffset;
-    if (input.sKey)
-      --memory.permanentState.yOffset;
-    if (input.dKey)
-      --memory.permanentState.xOffset;
+  if (input.wKey)
+    ++memory.permanentState.yOffset;
+  if (input.aKey)
+    ++memory.permanentState.xOffset;
+  if (input.sKey)
+    --memory.permanentState.yOffset;
+  if (input.dKey)
+    --memory.permanentState.xOffset;
 
   RenderGradient(offscreenBuffer.pixels, offscreenBuffer.width, offscreenBuffer.height, memory.permanentState.xOffset, memory.permanentState.yOffset);
 }
